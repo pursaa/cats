961c04cd371615321219af1e679bf0289950b2da
Erik Zakrzewski and Kristen Kulha <erik@>
2017-08-08 09:57:25 -0700

    Add images and text to index.html, add slideToggle and fadeToggle to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+  $(".clickable2").click(function() {
+    $("#slideToggle").slideToggle();
+  });
+});
