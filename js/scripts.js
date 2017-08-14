b1ef50d8333beefa9780940b14e8125a49ae270d
Debbie Michel and Kristen Kulha <kristen@>
2017-08-14 13:08:37 -0700

    Fix JS code to work when text is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
-  $(".clickable1").click(function() {
-    $("#fadeToggle").fadeToggle();
-  });
-  $(".clickable2").click(function() {
-    $("#slideToggle").slideToggle();
-  });
+  // $(".clickable1").click(function() {
+  //   $("#fadeToggle").fadeToggle();
+  // });
+  // $(".clickable2").click(function() {
+  //   $("#slideToggle").slideToggle();
+  // });
+
 
   var clickables = ["clickable1" , "clickable2"];
-  clickables.forEach(function(clickable){
-    $("clickable#" + clickable).click(function() {
-      $("#fadeToggle").fadeToggle();
-      $("#slideToggle").slideToggle();
-
-    });
-
-  // $("button#light").click(function(){
-  //   $("body").removeClass();
-  //   $("body").addClass("light");
-  // });
-  // $("button#dark").click(function(){
-  //   $("body").removeClass();
-  //   $("body").addClass("dark");
-  // });
-
+    clickables.forEach(function(clickable){
+      $("." + clickable).click(function() {
+        // $("#" + clickable).fadeToggle();
+        $("#" + clickable).slideToggle();
+  });
+});
 
   var buttons = ["light", "dark"];
   buttons.forEach(function(button){
@@ -31,5 +22,12 @@ $(document).ready(function() {
       $("body").addClass(button);
     });
   });
-
 });
+// $("button#light").click(function(){
+//   $("body").removeClass();
+//   $("body").addClass("light");
+// });
+// $("button#dark").click(function(){
+//   $("body").removeClass();
+//   $("body").addClass("dark");
+// });
